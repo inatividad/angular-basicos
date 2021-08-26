d062bb1577b80774182efb5a942175bd4a208e52
Iñigo Natividad <inatividad@minsait.com>
2021-08-26 10:04:27 +0200

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk'];
+
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void {
+
+    this.heroeBorrado = this.heroes.shift() || '';
+    
+  }
+
+  hayHeroeBorrado(): boolean {
+    return this.heroeBorrado != '';
+    console.log("hayBorrado");
+  }
+
+}
